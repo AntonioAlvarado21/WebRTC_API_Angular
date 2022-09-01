2b3f56baf82107d75240f474de73baf3544838bf
AntonioAlvarado21 <jcruza1300@alumno.ipn.mx>
2022-09-01 15:10:15 -0500

    Initial API Settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule, ReactiveFormsModule,
+    BrowserAnimationsModule, MatFormFieldModule, MatInputModule,
     AppRoutingModule
   ],
   providers: [],
